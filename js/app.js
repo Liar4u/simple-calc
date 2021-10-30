f0cf00c4c18bdc302933803e2aec2283e7530506
Liar4u <pavlenkoliar@gmail.com>
2021-10-30 08:10:43 +0300

    add buttons render and init function

--- js/app.js
+++ js/app.js
@@ -52,3 +52,24 @@ const buttons = [
     'green',
   ],
 ];
+
+const buttonsGrid = document.querySelector('.buttons');
+
+init();
+
+function init() {
+  for (let i = 0; i < buttons[0].length; i++) {
+    const button = buttons[0][i];
+    const style = buttons[1][i];
+
+    initButton(button, style);
+  }
+}
+
+function initButton(button, style) {
+  const currentButton = document.createElement('div');
+  currentButton.setAttribute('class', 'button' + ' ' + style);
+  currentButton.innerHTML = button;
+
+  buttonsGrid.append(currentButton);
+}
